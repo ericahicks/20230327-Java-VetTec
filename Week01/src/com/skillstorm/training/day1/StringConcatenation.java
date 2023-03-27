ce663eefe3f7bd7b0015f4d28952f8e4d808b99a
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-03-27 14:11:40 -0400

    day 1 code examples

---
+++ Week01/src/com/skillstorm/training/day1/StringConcatenation.java
@@ -0,0 +1,15 @@
+package com.skillstorm.training.day1;
+
+public class StringConcatenation {
+	
+	public static void main(String[] args) {
+		System.out.println("Erica" + "Hicks"); // concatenation appends Hicks to Erica without space btwn
+		System.out.println("Erica " + "Hicks");
+		
+		System.out.print("bananaas, ");
+		System.out.print("milk, ");
+		System.out.println("toothpaste");
+		System.out.println("---done---");
+	}
+
+}
