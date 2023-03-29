47e78536f73b170295cf16f1aab058b5a38a54b0
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-03-29 16:51:42 -0400

    day 3 modulus examples

--- Week01/src/com/skillstorm/training/day3/ModulusExamples.java
+++ Week01/src/com/skillstorm/training/day3/ModulusExamples.java
@@ -2,6 +2,23 @@ package com.skillstorm.training.day3;
 
 public class ModulusExamples {
 	
-	
+	public static void main(String[] args) {
+		// % modulus - gives the remainder from integer division
+		// if I do 3 / 2 = 1.5 aka 1 remainder 1 
+		// so 3 mod 2 = 1 NOT 1.5
+		
+		// Example: years and months
+		int totalMonths = 18;
+		// How may years and months is this?
+		int years = 18 / 12;
+		int months = 18 % 12;
+		System.out.println(totalMonths + " months is " + years + " years and " + months + " months");
+		
+		
+		// Example: what time will it be in 71 hours?
+		int now = 1400;
+		int timeIn71Hours = (now + 7100) % 2400;
+		System.out.println(timeIn71Hours);
+	}
 
 }
