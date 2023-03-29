a9b1474809aaded148f595c83976396408534b44
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-03-29 17:00:21 -0400

    day 3 adding modulus example 3

--- Week01/src/com/skillstorm/training/day3/ModulusExamples.java
+++ Week01/src/com/skillstorm/training/day3/ModulusExamples.java
@@ -7,7 +7,7 @@ public class ModulusExamples {
 		// if I do 3 / 2 = 1.5 aka 1 remainder 1 
 		// so 3 mod 2 = 1 NOT 1.5
 		
-		// Example: years and months
+		// Example1: years and months
 		int totalMonths = 18;
 		// How may years and months is this?
 		int years = 18 / 12;
@@ -15,7 +15,7 @@ public class ModulusExamples {
 		System.out.println(totalMonths + " months is " + years + " years and " + months + " months");
 		
 		
-		// Example: what time will it be in 71 hours?
+		// Example2: what time will it be in 71 hours?
 		int now = 1400;
 		int timeIn71Hours = (now + 7100) % 2400;
 		System.out.println(timeIn71Hours);
@@ -27,6 +27,24 @@ public class ModulusExamples {
 		
 		// modulus: 16 mod 2 what is 16 / 2? 8 what is 16 - (8 * 2) = 16 - 16 = 0
 		//          x  mod y what is x / y? n  what is  x - (n * y)
+		
+		// Example3: find the row and column of box 9 in the below grid
+		
+		// -----------------------------
+		// | 0 | 1 | 2 | 3 | 4 | 5 | 6 | <--- row: 0
+		// -----------------------------
+		// | 7 | 8 | 9 | 10| 11| 12| 13| <--- row: 1
+		// -----------------------------
+		// | 14| 15| 16| 17| 18| 19| 20| <--- row: 2
+		// -----------------------------
+		
+		int boxNum = 9;
+		int numRows = 3;
+		int numCols = 7;
+		
+		int row = boxNum / numCols;
+		int col = boxNum % 7;
+		System.out.println("Box #" + boxNum + " is in row " + row + ", col " + col);
 	}
 
 }
