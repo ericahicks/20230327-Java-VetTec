99de627d3efe4d59c60127bfef84506df6d7ab4f
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-04-04 12:27:12 -0400

    day 2 morning material

---
+++ Week02/src/com/skillstorm/training/day2/ReturnTypeExamples.java
@@ -0,0 +1,63 @@
+package com.skillstorm.training.day2;
+
+public class ReturnTypeExamples {
+	
+	public static void main(String[] args) {
+		add(1, 2); // I have not done anything with the return value
+		
+		// Option 1: store the return value to use later
+		int sum = add(3, 2);
+		
+		// Option 2: hand the return value directly to another method
+		System.out.println(add(100, 200));
+		
+		System.out.println(subtract(10, 20));
+		
+		System.out.println(multiply(2, 3));
+		
+		System.out.println();
+		
+		System.out.println(divide(1, 2)); // java tries to find an exact match for the parameters
+		System.out.println(divide(1f, 2)); // java will wide cast to match the float, float parameters for us
+		System.out.println(divide(1, 2f));
+		System.out.println(divide(1f, 2f));
+		
+//		System.out.println(divide(1, 0)); // causes error cannot have an int of infinity
+		System.out.println(divide(1f, 0f)); // prints out infinity
+		
+		// Can I use printf? Yes, but match the number type returned
+//		System.out.printf("%d", divide(5, 2)); // returns 2 when int division used
+////		System.out.printf("%d", divide(5.0f, 2.0f)); // returns 2 when int division used
+//		
+//		System.out.printf("%.1f", divide(5.0f, 2)); // returns 2 when int division used
+//		System.out.println(divide(5, 2f)); // returns 2 when int division used
+		
+		
+		// TODO write the methods multiply and divide and call them here. Put your a screenshot in the chat.
+	}
+	
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	public static int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public static float divide(float x, float y) {
+		return x / y;
+	}
+	
+	public static int divide(int x, int y) {
+		return x / y;
+	}
+	
+	// TODO write a method that returns a true if the number is even
+	// TODO write a method that returns a true if the age is greater than or equal to 21
+	// TODO write a method that returns a true if the age is a teenager
+
+}
