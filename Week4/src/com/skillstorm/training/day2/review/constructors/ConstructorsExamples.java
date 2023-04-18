f3cc6c7811dd856dfc2da06023e1e9a0b207571b
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-04-18 10:27:30 -0400

    day2 constructor examples

--- Week4/src/com/skillstorm/training/day2/review/constructors/ConstructorsExamples.java
+++ Week4/src/com/skillstorm/training/day2/review/constructors/ConstructorsExamples.java
@@ -1,10 +1,20 @@
 package com.skillstorm.training.day2.review.constructors;
 
 public class ConstructorsExamples {
+	// Points to take away
+	// 1. all constructors have a hidden call to super
+	// 2. the parent class of any class we don't specify the parent of
+	//      is Object
+	// 3. Before a constructor runs for the first time,
+	//      the classLoader fetches all the info about the class
+	//      and runs the static blocks
+	// 4. static properties are set up first before the construtor runs the first time
+	//      and only once
 	
 	public static void main(String[] args) {
-		Animal a = new Ostrich();
-//		Animal p = new Penguin();
+//		Bird a = new Ostrich(); 
+		Penguin p = new Penguin();
+		Penguin p2 = new Penguin();
 	}
 
 }
