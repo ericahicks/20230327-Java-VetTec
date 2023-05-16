24970c62c17787ddd249251564a32a1870e25a78
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-16 10:41:17 -0400

    findbiggest functional interface example

---
+++ Week8/src/com/skillstorm/training/day1/Action.java
@@ -0,0 +1,8 @@
+package com.skillstorm.training.day1;
+
+@FunctionalInterface // This allows me to use a lambda instead of an instance of a class that implements this interface
+public interface Action {
+	
+	void act();
+
+}
