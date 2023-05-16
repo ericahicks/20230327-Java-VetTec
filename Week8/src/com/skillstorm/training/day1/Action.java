85d8d6f896e399b5f80e020c6f14adeb1943f06f
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-16 11:12:13 -0400

    examples of functional interfaces and comments with lambdas

--- Week8/src/com/skillstorm/training/day1/Action.java
+++ Week8/src/com/skillstorm/training/day1/Action.java
@@ -6,3 +6,32 @@ public interface Action {
 	void act();
 
 }
+
+class DoNothing implements Action {
+	public void act() { System.out.println("Not doing anything"); }
+}
+
+// Example:
+// () -> System.out.println("Not doing anything");
+
+class Repeater implements Action {
+	public void act() {
+		String str = "dun-dun-dun ";
+		for (int i = 0; i < 10; i++) { // these only work at the end of a line of code
+			/* This is a comment in the front of the line*/System.out.print(str);
+		}
+	}
+}
+
+// example :
+/*
+() -> {
+
+   String str = "dun-dun-dun ";
+
+   for (int i = 0; i < 10; i++) {
+	   System.out.print(str);
+   }
+
+}
+*/
--- Week8/src/com/skillstorm/training/day2/FunctionalInterfacesReview2.java
+++ Week8/src/com/skillstorm/training/day2/FunctionalInterfacesReview2.java
@@ -6,40 +6,45 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FunctionalInterfacesReview2 {
-
+	static List<Integer> nums = new ArrayList<>();
 	// Example 1: Find number based on given criteria
-	public static Integer findNumberBasedOnCriteria(FindBiggest<Integer> fb) { // FindBiggest is a target type that could take a lambda!
-		List<Integer> nums = new ArrayList<>();
-		Scanner in = new Scanner(System.in);
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Please enter a whole number: ");
-			Integer num = in.nextInt();
-			nums.add(num);
-		}
-		in.close();
+	public static Integer findNumberBasedOnCriteria(Finder<Integer> fb) { // Finder is a target type that could take a lambda!
 		return fb.compare(nums);
 	}
 
 	public static void main(String[] args) {
-		// Create an instance of the class implementing FindBiggest
-		FindBiggestInteger fb = new FindBiggestInteger();
-//		FindBiggest<Integer> fb2 = new FindBiggestInteger(); // polymorphism lets me use a super type
-		Integer biggest = findNumberBasedOnCriteria(fb);
-		System.out.println(biggest);
+//		FindSmallestInteger fb2 = new FindSmallestInteger();
+//		Integer smallest = findNumberBasedOnCriteria(fb2);
+//		System.out.println("smallest is " + smallest);
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Please enter a whole number: ");
+			Integer num = in.nextInt();
+			in.nextLine();
+			nums.add(num);
+		}
+		in.close();
+		FindMedianInteger fb3 = new FindMedianInteger();
+		Integer median = findNumberBasedOnCriteria(fb3);
+		System.out.println("Median is " + median);
+		
+		FindSmallestInteger fb2 = new FindSmallestInteger();
+		Integer smallest = findNumberBasedOnCriteria(fb2);
+		System.out.println("smallest is " + smallest);
 	}
 
 }
 
 // Functional Interface
 @FunctionalInterface
-interface FindBiggest<T> {
+interface Finder<T> {
 
 	T compare(List<T> values);
 
 }
 
 // Implementing Class that compares Integers
-class FindBiggestInteger implements FindBiggest<Integer> {
+class FindSmallestInteger implements Finder<Integer> {
 
 	/**
 	 * Takes in any number of Integers, loops through and returns the biggest
@@ -50,11 +55,41 @@ class FindBiggestInteger implements FindBiggest<Integer> {
 	 */
 	@Override
 	public Integer compare(List<Integer> values) {
-		Integer largest = Integer.MIN_VALUE;
+		Integer largest = Integer.MAX_VALUE;
 		for (Integer num : values) { // loop through
-			largest = Math.max(largest, num);
+			largest = Math.min(largest, num);
 		}
 		return largest;
 	}
 
 }
+
+//Implementing Class that compares Integers
+class FindMedianInteger implements Finder<Integer> {
+
+	/**
+	 * Takes in any number of Integers, loops through and returns the biggest
+	 * Integer
+	 * 
+	 * @param The values to compare
+	 * @return The biggest Integer in the list
+	 */
+	@Override
+	public Integer compare(List<Integer> values) {
+		// median - the middle number (or the average of the two middle numbers)
+		// sort list
+		values.sort(null);
+		// if odd number of elements return the middle
+		if (values.size() % 2 == 1) {
+			// middle element of 1, 2, 3, 4, 5 is 3 at index 2
+			return values.get(values.size() / 2);
+		} else {
+			// middle elements of [1, 2, 3, 4] are 2 and 3 at index 1 and 2 
+			int elem1 = values.get(values.size() / 2);
+			int elem2 = values.get(values.size() / 2 - 1);
+			return (elem1 + elem2 ) / 2;
+		}
+	}
+
+}
+
