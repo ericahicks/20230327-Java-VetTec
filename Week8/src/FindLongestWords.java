1a4366a00ff07e71bfb606f7d2c017898d6db945
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-17 16:18:05 -0400

    examples reading and writing to file;'

---
+++ Week8/src/FindLongestWords.java
@@ -0,0 +1,18 @@
+
+public class FindLongestWords {
+	
+	// TODO Challenge
+	// Find the longest word in the words_alpha.txt
+	
+	// Problem 1:
+	// How many letters does the longest word have?
+	
+	
+	// Bonus 1:
+	// What is the longest word? 
+	
+	
+	// Bonus 2:
+	// Write the longest word(s) to a file
+
+}
