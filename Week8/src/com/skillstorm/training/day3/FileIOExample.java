63254011071602b00af50eda54672baf18361b40
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-17 12:05:19 -0400

    moving the try with resources example into a separate file

--- Week8/src/com/skillstorm/training/day3/FileIOExample.java
+++ Week8/src/com/skillstorm/training/day3/FileIOExample.java
@@ -21,40 +21,8 @@ public class FileIOExample {
 			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// try-with-resources blocks -- can only be used with objects that implement AutoCloseable
-		// instead of try-catch-finally
-//		try (Scanner in = new Scanner(System.in); // 
-//				Stuff s = new Stuff()) { // calls the close methods for us, in the opposite order the objects were created
-//			in.nextLine();
-//			throw new NullPointerException();
-//		} catch (Exception e) {
-//			System.out.println("exception was thrown");
-//		}
+
 	}
 
-}
-class Stuff implements AutoCloseable {
-
-	@Override
-	public void close() throws Exception {
-		System.out.println("Closing the stuff");
-		
-	}
-	
 }
 
---
+++ Week8/src/com/skillstorm/training/day3/TryWithResourcesExample.java
@@ -0,0 +1,44 @@
+package com.skillstorm.training.day3;
+
+import java.util.Scanner;
+
+public class TryWithResourcesExample {
+	public static void main(String[] args) throws Exception {
+		// try-with-resources blocks 
+		//  -- can be used instead of try-catch-finally to close resources
+		//  -- can only be used with objects that implement AutoCloseable
+		//  -- Java calls the close methods for us, in the opposite order the objects were created
+		//  -- catch and finally blocks run AFTER the close methods are called
+		try (Scanner in = new Scanner(System.in); // 
+				Stuff s = new Stuff();
+				MoreStuff s2 = new MoreStuff()) { // 
+			throw new NullPointerException();
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
+//			s.close(); // s has already been closed
+		} finally {
+//			s.close(); // s has already been closed
+		}
+	}
+}
+
+
+class Stuff implements AutoCloseable {
+	
+	@Override
+	public void close() throws Exception {
+		System.out.println("Closing the stuff");
+		
+	}
+	
+}
+
+class MoreStuff implements AutoCloseable {
+	
+	@Override
+	public void close() throws Exception {
+		System.out.println("Closing the more stuff");
+		
+	}
+	
+}
