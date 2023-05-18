fcb20f64bd62b601d3a9e6a4354521a158f65972
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-18 06:18:23 -0400

    longest words

--- Week8/src/FindLongestWords.java
+++ Week8/src/FindLongestWords.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FindLongestWords {
+	
+	public static void main(String[] args) throws IOException {
+//		int count = 0;
+//		String longest = "";
+		// file located at root level moby-dick.txt
+				try (BufferedReader in = new BufferedReader(
+						new FileReader(new File("words_alpha.txt")));
+						BufferedWriter out = new BufferedWriter(
+								new FileWriter(new File("long_words.txt")))) {
+					String s;
+					while ((s = in.readLine()) != null) {
+						
+						if (s.length() > 27 ) {
+							out.write(s);
+							out.newLine();
+						}
+					}
+				}
+//				System.out.println("longest is " + longest + " with " + count + " letters");
+	}
 	
 	// TODO Challenge
 	// Find the longest word in the words_alpha.txt
