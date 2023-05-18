f4eb4b38949c5f4eaad0e1062c7bb09dabad8326
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-18 16:13:59 -0400

    formatting dates numbers and currencies

---
+++ Week8/src/com/skillstorm/training/day4/Challenge.java
@@ -0,0 +1,12 @@
+package com.skillstorm.training.day4;
+
+public class Challenge {
+	// Create a custom Person class with a firstName, lastName, and dob (date of birth)
+	// write the data to a csv file
+
+	// Bonus:
+	// Read the data back from the file
+	// Find the oldest person
+	
+	// Find the age range between the oldest and youngest person
+}
