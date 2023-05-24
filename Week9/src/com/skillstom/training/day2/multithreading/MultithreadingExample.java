15657196a40c4359ad9fc2c0384b13713c9800bb
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-24 11:58:13 -0400

    synchronized with lock

--- Week9/src/com/skillstom/training/day2/multithreading/MultithreadingExample.java
+++ Week9/src/com/skillstom/training/day2/multithreading/MultithreadingExample.java
@@ -1,38 +1,63 @@
 package com.skillstom.training.day2.multithreading;
+
 public class MultithreadingExample {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		Universe u = Universe.getInstance();
-		
-		Thread t1 = new Thread(() -> System.out.println("t1"));
-		t1.start();
 
-		Thread t2 = new Thread(() -> System.out.println("t2"));
+		Thread t1 = new Thread(() -> {
+			System.out.println("t1");
+			for (int i = 0; i < 10; i++) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) { }
+				u.add();
+			}
+		});
+
+		Thread t2 = new Thread(() -> {
+			System.out.println("t2");
+			for (int i = 0; i < 10; i++) {
+				try {
+					Thread.sleep(90);
+				} catch (InterruptedException e) { }
+				u.add();
+			}
+		});
+		t1.start();
 		t2.start();
-		
-		t1.join(); // this tells the main thread to wait for t1 before continueing
+
+		t1.join(); // this tells the main thread to wait for t1 before continue
 		t2.join();
-		System.out.println("main");
-		
+		System.out.println("main done with " + u.getNumSolarSystems() + " solar systems");
+
 		// TODO put in the chat what you get from printing
 	}
 
 }
 
 class Universe {
-	
-	private int numSolarSystems;                             // to use lazy initialization:
-	private static volatile Universe instance; // = new Universe(); // where can I do this instead?
-	// atomic access = read and write must complete before another read can happen
-	private Universe() { }
-	
-	public synchronized static Universe getInstance() {
+
+	private int numSolarSystems; // to use lazy initialization:
+	private volatile static Universe instance; // = new Universe(); // where can
+												// I do this instead?
+	// atomic access = read and write must complete before another read can
+	// happen
+	private static Object keyToTheUniverse = new Object(); // mutex acts like a lock
+
+	private Universe() {
+	}
+
+	public static Universe getInstance() {
 		// only run new Universe once
-		if (instance == null)
+		if (instance == null) {
+			synchronized (keyToTheUniverse) {
 			instance = new Universe();
+			}
+		}
 		return instance;
 	}
-	
+
 	public int getNumSolarSystems() {
 		return numSolarSystems;
 	}
