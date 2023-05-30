8f57b0912cb44cc6ca0904d0a5f8360c6af07f0c
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-30 12:22:38 -0400

    enthuware review comments cleaner

--- Week10/src/com/skillstorm/day2/EnthuwareReview.java
+++ Week10/src/com/skillstorm/day2/EnthuwareReview.java
@@ -3,23 +3,30 @@ package com.skillstorm.day2;
 public class EnthuwareReview {
 	// 13
 
+	//////////////////////////////////////////////////////////////////////////////
 	// 14 Loops
-	// Reserved words: for, while, do, if, else, modifiers, primitive types,
+	// Reserved words: cannot be used as labels
+	// ----------------
+	// for, while, do, if, else, modifiers, primitive types,
 	// try, catch, finally
-	// continue, break, goto, strictfp, const, assert
+	// continue, break, switch, case,
+	// goto, strictfp, const, assert
+	// 
 	// NOT reserved are all the names of the built-in classes like String,
 	// Integer, Character, etc.
+	
 //	public static void main(String[] args) {
 //		int i = 0, j = 0;;
 //	    /* for  :*/ 
-//		outer: for(i = 0; i< 10; i++){ // reserved words in java that cannot be used
-//	         for (j = 0; j< 10; j++){  // for variable names or label names
-//	              if ( i + j > 10 )  break outer; //for;
+//		outer: for(i = 0; i< 10; i++){ 
+//	         for (j = 0; j< 10; j++){  
+//	              if ( i + j > 10 )  break outer; //for; // for is not a valid loop label
 //	         }
 // 	         System.out.println( "hello i " + i + ", j " + j);
 //	      }
 //	}
 
+	///////////////////////////////////////////////////////////////////////////////////
 	// 17 Inheritance and constructors
 	// - If we do not provide a constructor, Java gives us a default constructor
 	// with no-args
@@ -29,6 +36,7 @@ public class EnthuwareReview {
 	// - If we don't call super, automatically it gets called for us using
 	// no-arguments
 	// - A constructor does not have a return type
+	
 //	public static void main(String[] args) {
 //		B b = new B();
 //	}
@@ -41,36 +49,38 @@ public class EnthuwareReview {
 
 	// 43
 
+	//////////////////////////////////////////////////////////////////////////////
 	// 48
 	// break = breaks the loop it is in (not any outer loops) stopping all the
 	// break label = breaks the labeled loop (not any other loops from it)
 	// rest of the iterations
 	// continue = continue only skips the current interation (all the rest of
 	// the iterations run)
-	public static void main(String[] args) {
-		int counter = 0;
-		outer: for (int i = 0; i < 3; i++) {
-			middle: for (int j = 0; j < 3; j++) {
-				inner: for (int k = 0; k < 3; k++) {
-					/*
-					 * i j k
-					 * 0 0 0 +
-					 * 0 0 1 nope
-					 * 1 0 0 +
-					 * 1 0 1 nope
-					 * 2 0 0 +
-					 * 2 0 1 nope
-					 * 
-					 */
-					if (k > 0) {
-						break middle;
-					}
-					counter++;
-				}
-			}
-		}
-		System.out.println(counter);
-	}
+	
+//	public static void main(String[] args) {
+//		int counter = 0;
+//		outer: for (int i = 0; i < 3; i++) {
+//			middle: for (int j = 0; j < 3; j++) {
+//				inner: for (int k = 0; k < 3; k++) {
+//					/*
+//					 * i j k
+//					 * 0 0 0 +
+//					 * 0 0 1 nope
+//					 * 1 0 0 +
+//					 * 1 0 1 nope
+//					 * 2 0 0 +
+//					 * 2 0 1 nope
+//					 * 
+//					 */
+//					if (k > 0) {
+//						break middle;
+//					}
+//					counter++;
+//				}
+//			}
+//		}
+//		System.out.println(counter);
+//	}
 
 }
 
