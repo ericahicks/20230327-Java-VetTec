844dfd98a32a3dc2177b1ed16d063d3e3b926325
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-06-23 14:56:44 -0400

    boostarp and asynch example

--- Week13/week13-front-end/day4/05selecting.js
+++ Week13/week13-front-end/day4/05selecting.js
@@ -14,23 +14,33 @@
  * 
  */
 let div1 = document.getElementById("1");
-let div2 = document.getElementsByClassName("dark-mode");
+let div2 = document.getElementsByClassName("dark-mode").item(0); // NOTE: this is a list so get the first 
 let h1 = document.querySelector("h1");
 let btn = document.getElementById("myBtn");
+let btn2 = document.getElementById("btnDark");
+let btn3 = document.getElementById("btnLight");
 // let div1 = document.querySelector("#1");
-// let div2 = document.querySelector(".dark-mode");
+// let div2 = document.querySelector(".dark-mode:first-of-type");
 
 // Add an event listener to run my code when the button is clicked
 btn.addEventListener('click', () => {
     // And change the font size and color of div1.
     div1.style.fontSize = "2em";
     div1.style.color = "lightcoral";
+});
 
+btn2.addEventListener('click', () => {
+    // Remove the dark-mode class from div2
+    div2.classList.add("dark-mode"); // there is also .toggle 
+});
+
+btn3.addEventListener('click', () => {
     // Remove the dark-mode class from div2
     div2.classList.remove("dark-mode");
 });
 
 console.log(div1);
 console.log(div2);
+console.log(div2.classList)
 console.log(h1);
 
---
+++ Week13/week13-front-end/day4/06asynch.js
@@ -0,0 +1,10 @@
+// JavaScript has some functions similar to Java's thread methods
+// we can delay execution of our functions or run them at set intervals
+// JAVASCRIPT IS NOT MULTITHREADED it uses  AN EVENT LOOP asynch functions get put on a queue 
+let numDiv = document.getElementById("num");
+//numDiv.innerHTML = "<b>this will be bold</b>"
+numDiv.textContent = "No value set";
+setTimeout(() => {
+    numDiv.textContent = "Whoohoo!";
+}, 3000); // code to run and how long to wait in miliseconds 
+
