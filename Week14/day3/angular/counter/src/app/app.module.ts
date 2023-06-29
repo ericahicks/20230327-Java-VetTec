3bdbb262fa1f42e7a7a092f6570e0ff8e53ea7c0
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-06-29 15:56:19 -0400

    day 4 week 14

--- Week14/day3/angular/counter/src/app/app.module.ts
+++ Week14/day3/angular/counter/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ChildComponent } from './child/child.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Week14/day3/angular/counter/src/app/children.service.ts
+++ Week14/day3/angular/counter/src/app/children.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,14 @@ export class ChildrenService {
 
   dummyData = ["Suzie", "Robert", "Bobby Jr", "Baby Girl", "Timmy"];
 
-  constructor() { }
+  url: string  = environment.apiUrl; // "localhost:8080/books";
+
+  constructor(private http: HttpClient) { }
 
   getDummyData(): Observable<string[]> { // observables allow us to asynchronusly get data by subscribing to them
     // just like in Java when we do 
     // new ArrayList<String>(Array.asList(1, 2 , 3))
-    return of<string[]>(this.dummyData); // get me an observable of strings from this data
+    // return of<string[]>(this.dummyData); // get me an observable of strings from this data
+    return this.http.get<string[]>(this.url);
   }
 }
