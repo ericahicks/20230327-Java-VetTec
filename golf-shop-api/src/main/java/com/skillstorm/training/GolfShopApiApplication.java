30d4661b42b0c0eda335c0714788f8ff9720bd08
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-07-11 16:16:29 -0400

    eureka clients

--- golf-shop-api/src/main/java/com/skillstorm/training/GolfShopApiApplication.java
+++ golf-shop-api/src/main/java/com/skillstorm/training/GolfShopApiApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class GolfApiApplication {
+public class GolfShopApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GolfApiApplication.class, args);
+		SpringApplication.run(GolfShopApiApplication.class, args);
 	}
 
 }
